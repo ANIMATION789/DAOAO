5e61017fa6600846c037c1ad127e96c8604ed36d
ANIMATION789 <83766277+ANIMATION789@users.noreply.github.com>
2023-04-09 18:41:06 +0100

    Create 6-print-money.js

--- scripts/6-print-money.js
+++ scripts/6-print-money.js
@@ -3,7 +3,7 @@ import sdk from "./1-initialize-sdk.js";
 (async () => {
   try {
     // This is the address of our ERC-20 contract printed out in the step before.
-    const token = await sdk.getContract("INSERT_TOKEN_ADDRESS", "token");
+    const token = await sdk.getContract("0x14AF592F46e559b059bBAeC0c3851a8c902bEE15", "token");
     // What's the max supply you want to set? 1,000,000 is a nice number!
     const amount = 1_000_000;
     // Interact with your deployed ERC-20 contract and mint the tokens!
@@ -11,7 +11,7 @@ import sdk from "./1-initialize-sdk.js";
     const totalSupply = await token.totalSupply();
 
     // Print out how many of our token's are out there now!
-    console.log("✅ There now is", totalSupply.displayValue, "$HOKAGE in circulation");
+    console.log("✅ There now is", totalSupply.displayValue, "Governance Token in circulation");
   } catch (error) {
     console.error("Failed to print money", error);
   }
